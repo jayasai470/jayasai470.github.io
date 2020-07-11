a5f38b0f977427ff00aab33f5a2fac1d6a2efb95
j.muppala <j.muppala@samsung.com>
2020-07-11 18:05:00 -0500

    typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import{ Framer } from './components/framer'
+import{ Framer } from './components/Framer'
 
 function App() {
   return (
