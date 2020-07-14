1c2357228cc7ce0f729f08d1b800809dda7fef20
j.muppala <j.muppala@samsung.com>
2020-07-14 02:23:44 -0500

    folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 // import ReactGA from 'react-ga';
 import './App.css';
-import Header from './Components2/Header';
-import Footer from './Components2/Footer';
-import About from './Components2/About';
-import Resume from './Components2/Resume';
-import Contact from './Components2/Contact';
-import Portfolio from './Components2/Portfolio';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
